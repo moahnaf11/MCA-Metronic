583c2475960f192f3125558b22087dabfc06a8f2
Ahnaf <mohammadahnaf123@gmail.com>
2025-07-22 13:49:27 +0400

    Feat: Use alert Dialog in EditableCell instead of Dialog Fix: Fix date times in creating auction to display errors
    Changed dialogs to use alert dialogs
    Fixed error messages and date time comparisons to be local instead of UTC

---
+++ src/lib/LocalDateTime.ts
@@ -0,0 +1,5 @@
+export function combineDateTime(dateStr: string, timeStr: string) {
+  const [year, month, day] = dateStr.split('-').map(Number);
+  const [hours, minutes] = timeStr.split(':').map(Number);
+  return new Date(year, month - 1, day, hours, minutes); // Local time
+}
